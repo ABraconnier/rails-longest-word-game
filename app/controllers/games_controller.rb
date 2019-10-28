c5f43fed94acc5007f41a52027ce2c24f0b6256a
Antoine Braconnier <antoine.braconnier@hotmail.com>
2019-10-28 16:59:03 +0100

    implemented score

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,26 +2,31 @@ require 'open-uri'
 require 'json'
 
 class GamesController < ApplicationController
+
+  @@score = 0
+
+
   def new
     @letters = 10.times.map { ('A'..'Z').to_a.sample }
   end
 
   def word_match?(input, grid)
     input.all? do |letter|
-      input.count(letter) >= grid.count(letter)
+      input.count(letter) <= grid.count(letter)
     end
   end
 
   def correct?(input, grid)
-    if word_match?(input, grid)
+    if !word_match?(input, grid)
       "Sorry but #{@word.upcase} can't be build out of #{@current_letters}"
     elsif @word_found['found'] == false
       "Sorry but #{@word.upcase} doesn't exist"
     else
+      @@score = @@score + input.length ** 2
+      session[:score] = @@score
       "Great job"
     end
   end
-
   def score
     @word = params[:word]
     word_array = @word.upcase.split('')
@@ -29,6 +34,7 @@ class GamesController < ApplicationController
     current_letters_array = @current_letters.split(' ')
     url = "https://wagon-dictionary.herokuapp.com/#{@word}"
     @word_found = JSON.parse(open(url).read)
-    @score = correct?(word_array, current_letters_array)
+    @result = correct?(word_array, current_letters_array)
+    @score = @@score
   end
 end
